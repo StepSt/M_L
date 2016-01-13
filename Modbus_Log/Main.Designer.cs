8f2eeaab72940f22164e706e7b2d07b26c9c1217
StepSt <legionst@gmail.com>
2016-01-13 16:25:24 +0500

    Добавил Простой интерфейс и библиотеку Modbus

--- Modbus_Log/Main.Designer.cs
+++ Modbus_Log/Main.Designer.cs
@@ -28,20 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtReadMessege = new System.Windows.Forms.TextBox();
+            this.btnReadRegister = new System.Windows.Forms.Button();
+            this.txtReadRegisterValue = new System.Windows.Forms.TextBox();
+            this.txtReadModbusAddress = new System.Windows.Forms.TextBox();
+            this.txtReadModbusCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnWriteModbusValue = new System.Windows.Forms.Button();
+            this.txtWriteModbusValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWriteModbusAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.IPAddress = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtReadMessege);
+            this.groupBox2.Controls.Add(this.btnReadRegister);
+            this.groupBox2.Controls.Add(this.txtReadRegisterValue);
+            this.groupBox2.Controls.Add(this.txtReadModbusAddress);
+            this.groupBox2.Controls.Add(this.txtReadModbusCount);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(11, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(301, 138);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Чтение";
+            // 
+            // txtReadMessege
+            // 
+            this.txtReadMessege.Location = new System.Drawing.Point(142, 105);
+            this.txtReadMessege.Name = "txtReadMessege";
+            this.txtReadMessege.Size = new System.Drawing.Size(158, 20);
+            this.txtReadMessege.TabIndex = 6;
+            // 
+            // btnReadRegister
+            // 
+            this.btnReadRegister.Location = new System.Drawing.Point(31, 67);
+            this.btnReadRegister.Name = "btnReadRegister";
+            this.btnReadRegister.Size = new System.Drawing.Size(69, 29);
+            this.btnReadRegister.TabIndex = 5;
+            this.btnReadRegister.Text = "Прочитать";
+            this.btnReadRegister.UseVisualStyleBackColor = true;
+            // 
+            // txtReadRegisterValue
+            // 
+            this.txtReadRegisterValue.Location = new System.Drawing.Point(156, 77);
+            this.txtReadRegisterValue.Name = "txtReadRegisterValue";
+            this.txtReadRegisterValue.ReadOnly = true;
+            this.txtReadRegisterValue.Size = new System.Drawing.Size(73, 20);
+            this.txtReadRegisterValue.TabIndex = 4;
+            this.txtReadRegisterValue.Text = "0";
+            // 
+            // txtReadModbusAddress
+            // 
+            this.txtReadModbusAddress.Location = new System.Drawing.Point(156, 34);
+            this.txtReadModbusAddress.Name = "txtReadModbusAddress";
+            this.txtReadModbusAddress.Size = new System.Drawing.Size(73, 20);
+            this.txtReadModbusAddress.TabIndex = 4;
+            this.txtReadModbusAddress.Text = "0";
+            // 
+            // txtReadModbusCount
+            // 
+            this.txtReadModbusCount.Location = new System.Drawing.Point(28, 34);
+            this.txtReadModbusCount.Name = "txtReadModbusCount";
+            this.txtReadModbusCount.Size = new System.Drawing.Size(73, 20);
+            this.txtReadModbusCount.TabIndex = 4;
+            this.txtReadModbusCount.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Адрес";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Усли что-то пошло не так";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnWriteModbusValue);
+            this.groupBox1.Controls.Add(this.txtWriteModbusValue);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtWriteModbusAddress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(11, 212);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 138);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Запись";
+            // 
+            // btnWriteModbusValue
+            // 
+            this.btnWriteModbusValue.Location = new System.Drawing.Point(40, 78);
+            this.btnWriteModbusValue.Name = "btnWriteModbusValue";
+            this.btnWriteModbusValue.Size = new System.Drawing.Size(69, 29);
+            this.btnWriteModbusValue.TabIndex = 5;
+            this.btnWriteModbusValue.Text = "Записать";
+            this.btnWriteModbusValue.UseVisualStyleBackColor = true;
+            // 
+            // txtWriteModbusValue
+            // 
+            this.txtWriteModbusValue.Location = new System.Drawing.Point(37, 45);
+            this.txtWriteModbusValue.Name = "txtWriteModbusValue";
+            this.txtWriteModbusValue.Size = new System.Drawing.Size(73, 20);
+            this.txtWriteModbusValue.TabIndex = 4;
+            this.txtWriteModbusValue.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Значение";
+            // 
+            // txtWriteModbusAddress
+            // 
+            this.txtWriteModbusAddress.Location = new System.Drawing.Point(165, 45);
+            this.txtWriteModbusAddress.Name = "txtWriteModbusAddress";
+            this.txtWriteModbusAddress.Size = new System.Drawing.Size(73, 20);
+            this.txtWriteModbusAddress.TabIndex = 4;
+            this.txtWriteModbusAddress.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Адрес";
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(9, 29);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(112, 20);
+            this.txtIPAddress.TabIndex = 4;
+            this.txtIPAddress.Text = "127.0.0.1";
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.AutoSize = true;
+            this.IPAddress.Location = new System.Drawing.Point(12, 9);
+            this.IPAddress.Name = "IPAddress";
+            this.IPAddress.Size = new System.Drawing.Size(55, 13);
+            this.IPAddress.TabIndex = 3;
+            this.IPAddress.Text = "IPAddress";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(330, 383);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.IPAddress);
             this.Name = "Main";
             this.Text = "Main";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtReadMessege;
+        private System.Windows.Forms.Button btnReadRegister;
+        private System.Windows.Forms.TextBox txtReadRegisterValue;
+        private System.Windows.Forms.TextBox txtReadModbusAddress;
+        private System.Windows.Forms.TextBox txtReadModbusCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnWriteModbusValue;
+        private System.Windows.Forms.TextBox txtWriteModbusValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWriteModbusAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.Label IPAddress;
     }
 }
 
--- Modbus_Log/Main.cs
+++ Modbus_Log/Main.cs
@@ -5,9 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Sockets;
+using Modbus.Device;
+using Modbus;
 using NLog;
 
 namespace Modbus_Log
@@ -19,6 +20,45 @@ namespace Modbus_Log
         {
             InitializeComponent();
             _logger.Log(LogLevel.Info, "Запуск приложения");
+        }
+        private void btnReadRegister_Click(object sender, EventArgs e)
+        {
+            using (TcpClient client = new TcpClient(txtIPAddress.Text, 502))
+            {
+                ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+                ushort startAddress = Convert.ToUInt16(txtReadModbusAddress.Text);
+                ushort numInputs = Convert.ToUInt16(txtReadModbusCount.Text);
+
+                try
+                {
+                    ushort[] inputs = master.ReadHoldingRegisters(startAddress, numInputs);
+                    //SlaveException MNMess = new SlaveException();
+                    //txtReadMessege.Text = MNMess.Message;
+
+                    foreach (ushort us in inputs)
+                        txtReadRegisterValue.Text = us.ToString();
+                }
+                catch (SlaveException ex)
+                {
+                    txtReadMessege.Text = Convert.ToString(ex.FunctionCode) + ";";
+                    txtReadMessege.Text += Convert.ToString(ex.SlaveExceptionCode) + ";";
+                }
+            }
+        }
+
+        private void btnWriteModbusValue_Click(object sender, EventArgs e)
+        {
+            using (TcpClient client = new TcpClient(txtIPAddress.Text, 502))
+            {
+                ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+                ushort startAddress = Convert.ToUInt16(txtWriteModbusAddress.Text);
+                ushort numInputs = Convert.ToUInt16(txtWriteModbusValue.Text);
+
+                master.WriteSingleRegister(startAddress, numInputs);
+
+            }
         }
     }
 }
