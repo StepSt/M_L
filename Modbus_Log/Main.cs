a293d269d092eade43416b2b2b62c9bee4fbbf08
StepSt <legionst@gmail.com>
2016-01-13 16:07:13 +0500

    Создал проект

---
+++ Modbus_Log/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NLog;
+
+namespace Modbus_Log
+{
+    public partial class Main : Form
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        public Main()
+        {
+            InitializeComponent();
+            _logger.Log(LogLevel.Info, "Запуск приложения");
+        }
+    }
+}
