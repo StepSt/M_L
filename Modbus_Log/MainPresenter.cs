e68964a3f6b1838cdf9fd2c5bfc5e10b7d1a8948
StepSt <legionst@gmail.com>
2016-01-14 16:36:06 +0500

    добавил все то нет

---
+++ Modbus_Log/MainPresenter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using Modbus.Device;
+using Modbus;
+using Modbus_log.BL;
+
+namespace Modbus_Log
+{
+    class MainPresenter
+    {
+        private readonly IMainForm _view;
+        private readonly ITCP _tcp;
+
+        public MainPresenter (IMainForm view, ITCP tcp)
+        {
+            _view = view;
+            _tcp = tcp;
+
+            _view.ReadBtnClick += _view_ReadBtnClick;
+        }
+
+        void _view_ReadBtnClick(object sender, EventArgs e)
+        {
+            string view_IPAddress = _view.IPAddress;
+            string view_startAddress = _view.startAddress;
+            string view_numInputs = _view.numInputs;
+            string view_slaveID = _view.slaveID;
+            int view_cbTypes = _view.cbTypes;
+
+            using (TcpClient client = new TcpClient(view_IPAddress, 502))
+            {
+
+                ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+                ushort startAddress = Convert.ToUInt16(view_startAddress);
+                ushort numInputs = Convert.ToUInt16(view_numInputs);
+                byte slaveID = Convert.ToByte(view_slaveID);
+        try
+            {
+                ushort[] inputs = master.ReadHoldingRegisters(slaveID, startAddress, numInputs);
+                switch (view_cbTypes)
+                {
+                    //case 0:
+                    //    foreach (ushort us in inputs)
+                    //        txtReadRegisterValue.Text = us.ToString();
+                    //    break;
+                    case 1:
+                        // Из 2-х прочитанных ushort собираем 1 float
+                        float value = _tcp.GetFloatTcp(inputs);
+                        _view.ReadFloat(value);
+                        break;
+                    case 2:
+                        // Из 2-х прочитанных ushort собираем 1 float inverse
+                        value = _tcp.GetFloatInverseTcp(inputs);
+                        _view.ReadFloat(value);
+                        break;
+                    default:
+                        MessageBox.Show("Выберете тип переменной!");
+                        break;
+                }
+            }
+        catch (IndexOutOfRangeException)
+        {
+            MessageBox.Show("Размер не может быть =1");
+        }
+
+
+            }
+        }
+    }
+}
---
+++ Modbus_log.BL/TCP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+using Modbus.Device;
+using Modbus;
+using NLog;
+
+namespace Modbus_log.BL
+{
+    public interface ITCP
+    {
+        float GetFloatTcp(ushort[] inputs);
+        float GetFloatInverseTcp(ushort[] inputs);
+    }
+    public class TCP:ITCP
+    {
+        
+        public float GetFloatTcp( ushort[] inputs )
+        {
+            float value = BitConverter.ToSingle(BitConverter.GetBytes(inputs[0]).Concat(BitConverter.GetBytes(inputs[1])).ToArray(), 0);
+            return value;
+        }
+        public float GetFloatInverseTcp(ushort[] inputs)
+        {
+            float value = BitConverter.ToSingle(BitConverter.GetBytes(inputs[1]).Concat(BitConverter.GetBytes(inputs[0 ])).ToArray(), 0);
+            return value;
+        }
+    }
+}
